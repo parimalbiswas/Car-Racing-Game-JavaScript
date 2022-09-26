9a30113e574df04f739c53e638676a127268e111
PARIMAL BISWAS <biswasparimal01@gmail.com>
2022-09-26 14:06:01 +0530

    .

--- script.js
+++ script.js
@@ -6,16 +6,29 @@ const gameArea = document.querySelector(".gameArea");
 
 
 
-startSceen.addEventListener("click",start);
+startSceen.addEventListener("click", start);
 function start() {
+
+    gameArea.classList.remove("hide");
+    startSceen.classList.add("hide");
+
+
+    player.start = true;
     window.requestAnimationFrame(gamePlay);
+
+    let car = document.createElement("div");
+    car.setAttribute("class","car");
+    //car.innerText="Car I am";
+    gameArea.appendChild(car);
 }
 
-function gamePlay()
-{
+function gamePlay() {
     console.log("Game suru");
-    window.requestAnimationFrame(gamePlay);
- 
+    if (player.start) {
+        window.requestAnimationFrame(gamePlay);
+    }
+
+
 }
 
 
@@ -26,6 +39,8 @@ let keys = {
     ArrowLeft: false,
     ArrowRight: false
 }
+
+let player = {};
 
 document.addEventListener("keydown", keydown);
 document.addEventListener("keyup", keyup);
