da4c0be9a05a363d4ea500956ff4004a4c8e03a6
PARIMAL BISWAS <biswasparimal01@gmail.com>
2022-09-28 13:59:47 +0530

    .

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// 19:55
+
 const score = document.querySelector(".score");
 
 const startSceen = document.querySelector(".startSceen");
@@ -9,11 +11,13 @@ const gameArea = document.querySelector(".gameArea");
 startSceen.addEventListener("click", start);
 function start() {
 
-    gameArea.classList.remove("hide");
+    //gameArea.classList.remove("hide");
     startSceen.classList.add("hide");
+    gameArea.innerHTML = "";
 
 
     player.start = true;
+    player.score = 0;
     window.requestAnimationFrame(gamePlay);
 
     for (let index = 0; index < 10; index++) {
@@ -40,6 +44,20 @@ function start() {
     player.y = car.offsetTop;
 
 
+    for (let index = 0; index < 3; index++) {
+        let enemyCar = document.createElement("div");
+        enemyCar.setAttribute("class", "enemy");
+        enemyCar.style.background = "pink";
+
+        // enemyCar.y = (index * 150);
+        enemyCar.y = ((index + 1) * 350) * -1;
+
+        enemyCar.style.left = Math.floor(Math.random() * 350) + "px";
+        enemyCar.style.top = enemyCar.y + "px";
+        gameArea.appendChild(enemyCar);
+
+    }
+
     // console.log("Top Position ->"+car.offsetTop);
     // console.log("Left Position ->"+car.offsetLeft);
 }
@@ -48,23 +66,58 @@ function start() {
 function moveLines() {
     let lines = document.querySelectorAll(".lines");
     lines.forEach(function (elem) {
+
+        if (elem.y >= 800) {
+            elem.y -= 750;
+        }
         elem.y += player.speed;
-        roadLine.style.top = roadLine.y + "px";
+        elem.style.top = elem.y + "px";
     });
 
 }
 
-8: 27
+function endGame() {
+    player.start = false;
+    startSceen.classList.remove("hide");
+}
+
+
+
+function moveEnemy(car) {
+    let enemy = document.querySelectorAll(".enemy");
+
+    enemy.forEach(function (elem) {
+
+        if (isCollide(car, elem)) {
+            console.log("Hit");
+            endGame();
+        }
+
+
+        if (elem.y >= 850) {
+            elem.y = -300;
+            elem.style.left = Math.floor(Math.random() * 350) + "px";
+        }
+        elem.y += player.speed;
+        elem.style.top = elem.y + "px";
+    });
+
+}
+
+
 
 
 function gamePlay() {
-    console.log("Game suru");
+    //console.log("Game suru");
     let car = document.querySelector(".car");
 
     let road = gameArea.getBoundingClientRect();
-    console.log(road);
+    //console.log(road);
 
     if (player.start) {
+
+        moveLines();
+        moveEnemy(car);
 
         if (keys.ArrowUp && player.y > (road.top + 150)) {
             player.y -= player.speed;
@@ -76,7 +129,7 @@ function gamePlay() {
         if (keys.ArrowLeft && player.x > 0) {
             player.x -= player.speed;
         }
-        if (keys.ArrowRight && player.x < (road.width - 50)) {
+        if (keys.ArrowRight && player.x < (road.width - 71.5)) {
             player.x += player.speed;
         }
 
@@ -85,6 +138,10 @@ function gamePlay() {
 
 
         window.requestAnimationFrame(gamePlay);
+        console.log(player.score++);
+
+        player.score++;
+        score.innerText = "Score : " + player.score;
     }
 
 
@@ -99,7 +156,7 @@ let keys = {
     ArrowRight: false
 }
 
-let player = { speed: 9 };
+let player = { speed: 9, score: 0 };
 
 document.addEventListener("keydown", keydown);
 document.addEventListener("keyup", keyup);
@@ -110,13 +167,23 @@ document.addEventListener("keyup", keyup);
 function keydown(e) {
     e.preventDefault();
     keys[e.key] = true;
-    console.log(e.key);
-    console.log(keys);
+    //console.log(e.key);
+    //console.log(keys);
 }
 
 function keyup(e) {
     e.preventDefault();
     keys[e.key] = false;
-    console.log(e.key);
-    console.log(keys);
+    //console.log(e.key);
+    //console.log(keys);
+}
+
+
+function isCollide(a, b) {
+    aRact = a.getBoundingClientRect();
+    bRact = b.getBoundingClientRect();
+
+    return !((aRact.bottom < bRact.top) || (aRact.top > bRact.bottom)
+        || (aRact.right < bRact.left) || (aRact.left > bRact.right));
+
 }
